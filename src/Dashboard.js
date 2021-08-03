73ee29861d43a5e776e8205b4a94c350c27002f2
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2021-08-03 13:46:59 -0300

    criado 6 tipos de gráficos como exemplo

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,7 +1,89 @@
+import React, { useState } from "react";
+import Chart from "react-google-charts";
+
 import "./dashboard.css";
 
 function Dashboard() {
-    return <h1>dashboard</h1>;
+    const TITULO = "Quantidade de cadastros primeiro semestre";
+    const ANIMACAO = { duration: 1200, easing: "out", startup: true };
+    const [dados, setDados] = useState([
+        ["Mês", "Quantidades"],
+        ["Janeiro", 33],
+        ["Fevereiro", 68],
+        ["Março", 49],
+        ["Abril", 15],
+        ["Maio", 80],
+        ["Junho", 27],
+    ]);
+
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <Chart
+                width={"400px"}
+                height={"300px"}
+                chartType="PieChart"
+                data={dados}
+                options={{ title: TITULO }}
+            />
+
+            <Chart
+                width={"400px"}
+                height={"300px"}
+                chartType="PieChart"
+                data={dados}
+                options={{ title: TITULO, is3D: true }}
+            />
+
+            <Chart
+                width={"400px"}
+                height={"300px"}
+                chartType="PieChart"
+                data={dados}
+                options={{ title: TITULO, pieHole: 0.4 }}
+            />
+
+            <Chart
+                width={"400px"}
+                height={"300px"}
+                chartType="BarChart"
+                data={dados}
+                options={{
+                    title: TITULO,
+                    chartArea: { width: "50%" },
+                    hAxis: { title: "Quantidade" },
+                    vAxis: { title: "Mês" },
+                    animation: ANIMACAO,
+                }}
+            />
+
+            <Chart
+                width={"400px"}
+                height={"300px"}
+                chartType="LineChart"
+                data={dados}
+                options={{
+                    title: TITULO,
+                    hAxis: { title: "Quantidade" },
+                    vAxis: { title: "Mês" },
+                    animation: ANIMACAO,
+                }}
+            />
+
+            <Chart
+                width={"400px"}
+                height={"300px"}
+                chartType="AreaChart"
+                data={dados}
+                options={{
+                    title: TITULO,
+                    hAxis: { title: "Quantidade" },
+                    vAxis: { title: "Mês" },
+                    animation: ANIMACAO,
+                }}
+            />
+        </div>
+    );
 }
 
 export default Dashboard;
