f4072211230a04d3c89ff30ebcf668d5450624f6
Pedro Gutierres <pedro-gabriel1@hotmail.com>
2021-08-03 14:29:21 -0300

    adicionado atualização a cada 5 segundos dos dados para simular dinamismo nos gráficos

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Chart from "react-google-charts";
 
 import "./dashboard.css";
@@ -16,72 +16,102 @@ function Dashboard() {
         ["Junho", 27],
     ]);
 
+    // ciclos de vida do useEffect:
+    // inicial quando carrega a página
+    // update quando dados é modificado
+    // destruir componente para iniciar outro bloco de dados evitando looping infinito
+    //por isso o return no useEffect, o código ali dentro é executa somente depois do componente ser destuido
+    useEffect(() => {
+        function alterarDados() {
+            const dadosGraficos = dados.map((linha) => {
+                if (Number.isInteger(linha[1])) {
+                    linha[1] = Math.floor(Math.random() * 100);
+                }
+                return linha;
+            });
+            setDados(dadosGraficos);
+        }
+
+        const intervalId = setInterval(() => alterarDados(), 7000);
+
+        return () => {
+            //
+            clearInterval(intervalId);
+        };
+    }, [dados]);
+
     return (
-        <div>
-            <h1>Dashboard</h1>
-            <Chart
-                width={"400px"}
-                height={"300px"}
-                chartType="PieChart"
-                data={dados}
-                options={{ title: TITULO }}
-            />
+        <div className="container">
+            <h1>Dashboards</h1>
+            <div className="container-charts">
+                <div className="container-chart">
+                    <Chart
+                        width={"400px"}
+                        height={"300px"}
+                        chartType="PieChart"
+                        data={dados}
+                        options={{ title: TITULO }}
+                    />
 
-            <Chart
-                width={"400px"}
-                height={"300px"}
-                chartType="PieChart"
-                data={dados}
-                options={{ title: TITULO, is3D: true }}
-            />
+                    <Chart
+                        width={"400px"}
+                        height={"300px"}
+                        chartType="PieChart"
+                        data={dados}
+                        options={{ title: TITULO, is3D: true }}
+                    />
 
-            <Chart
-                width={"400px"}
-                height={"300px"}
-                chartType="PieChart"
-                data={dados}
-                options={{ title: TITULO, pieHole: 0.4 }}
-            />
+                    <Chart
+                        width={"400px"}
+                        height={"300px"}
+                        chartType="PieChart"
+                        data={dados}
+                        options={{ title: TITULO, pieHole: 0.4 }}
+                    />
+                </div>
 
-            <Chart
-                width={"400px"}
-                height={"300px"}
-                chartType="BarChart"
-                data={dados}
-                options={{
-                    title: TITULO,
-                    chartArea: { width: "50%" },
-                    hAxis: { title: "Quantidade" },
-                    vAxis: { title: "Mês" },
-                    animation: ANIMACAO,
-                }}
-            />
+                <div className="container-chart">
+                    <Chart
+                        width={"400px"}
+                        height={"300px"}
+                        chartType="BarChart"
+                        data={dados}
+                        options={{
+                            title: TITULO,
+                            chartArea: { width: "50%" },
+                            hAxis: { title: "Quantidade" },
+                            vAxis: { title: "Mês" },
+                            animation: ANIMACAO,
+                        }}
+                    />
 
-            <Chart
-                width={"400px"}
-                height={"300px"}
-                chartType="LineChart"
-                data={dados}
-                options={{
-                    title: TITULO,
-                    hAxis: { title: "Quantidade" },
-                    vAxis: { title: "Mês" },
-                    animation: ANIMACAO,
-                }}
-            />
+                    <Chart
+                        width={"400px"}
+                        height={"300px"}
+                        chartType="LineChart"
+                        data={dados}
+                        options={{
+                            title: TITULO,
+                            hAxis: { title: "Quantidade" },
+                            vAxis: { title: "Mês" },
+                            animation: ANIMACAO,
+                        }}
+                    />
 
-            <Chart
-                width={"400px"}
-                height={"300px"}
-                chartType="AreaChart"
-                data={dados}
-                options={{
-                    title: TITULO,
-                    hAxis: { title: "Quantidade" },
-                    vAxis: { title: "Mês" },
-                    animation: ANIMACAO,
-                }}
-            />
+                    <Chart
+                        width={"400px"}
+                        height={"300px"}
+                        chartType="AreaChart"
+                        data={dados}
+                        options={{
+                            title: TITULO,
+                            hAxis: { title: "Quantidade" },
+                            vAxis: { title: "Mês" },
+                            animation: ANIMACAO,
+                        }}
+                    />
+                </div>
+            </div>
         </div>
     );
 }
